3c7ecff6d3d7ff18cabd84c4511eaa3b6fad058d
carlos Garcia <Carlos Garcia>
2023-05-08 17:53:12 -0400

    agregue la funcion para capturar los datos del usuario

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public class program
 {
-public static void Main()
+public static void Main(string[] args)
 {
 
-Console.WriteLine("Hola mundo!");
+string nombre;
+double edad;
+double ID;
+
+Console.WriteLine("Bienvenido a la JCE");
+
+Console.WriteLine("Digite su ID");
+ID = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite su nombre");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Digite su edad");
+edad = Convert.ToDouble(Console.ReadLine());
 
 }
 
