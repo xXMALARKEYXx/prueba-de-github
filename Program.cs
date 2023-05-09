0e932ead880db8ef52636069465a4f5564ac0219
carlos Garcia <Carlos Garcia>
2023-05-08 23:45:39 -0400

    Se agrego la funcionalidad de guardar datos en una base de datos local

--- Program.cs
+++ Program.cs
@@ -1,30 +1,79 @@
-﻿using System;
+﻿using System.Data;
+using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
-
+namespace prueba_github
+{
 public class program
 {
 public static void Main(string[] args)
 {
+SqlConnection connection = new SqlConnection();
+connection.ConnectionString = @"Data Source=MSI\MSICI;Initial Catalog=datos;Integrated Security=True";
+connection.Open();
+ SqlTransaction transaction = null;
+  SqlCommand command = new SqlCommand();
+ command.Connection = connection;
+ usuarios usuarios = new usuarios();
+ string salir = "N";
+ Console.WriteLine("Registro");
 
-string nombre;
-double edad;
-double ID;
+    Console.WriteLine($"{connection.State}");
+
+    Console.WriteLine("");
 
 Console.WriteLine("Bienvenido a la JCE");
 
 Console.WriteLine("Digite su ID");
-ID = Convert.ToDouble(Console.ReadLine());
+usuarios.Cedula = Convert.ToInt32(Console.ReadLine());
+ command.CommandText = "SPRegistro";
+ command.Parameters.Clear();
+ command.Parameters.AddWithValue("@Cedula", usuarios.Cedula);
+command.CommandType = System.Data.CommandType.StoredProcedure;
 
 Console.WriteLine("Digite su nombre");
-nombre = Console.ReadLine();
+ usuarios.Nombre = Console.ReadLine();
 
 Console.WriteLine("Digite su edad");
-edad = Convert.ToDouble(Console.ReadLine());
+usuarios.edad = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+
+                        transaction = connection.BeginTransaction();
+                        command.Transaction = transaction;
+                        command.CommandText = "SPRegistro";
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@Cedula", usuarios.Cedula);
+                        command.Parameters.AddWithValue("@Nombre", usuarios.Nombre);
+                        command.Parameters.AddWithValue("@Edad", usuarios.edad);
+                         command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        int Respuesta = command.ExecuteNonQuery();
+
+                        Respuesta = command.ExecuteNonQuery();
+
+                        transaction.Commit();
+
+                        Console.WriteLine($"{Respuesta}");
 
 }
 
+catch(Exception error)
+{
+ transaction.Rollback();
+ 
+}
+
+
+                    Console.WriteLine("Quiere salir? Y/N");
+                    salir = Console.ReadLine();
+                    Console.Clear();
+
+}
+
+}
 }
---
+++ usuarios.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace prueba_github
+{
+    public class usuarios
+    {
+        public int Cedula { get; set;}
+
+        public string Nombre {get; set;}
+
+        public int edad { get; set;}
+
+
+    }
+}
